5c212f6cff2e9470a5a556557f6cd9f07df34579
Talha Acikgoz <tacikgoz@k2m08s07.42kocaeli.com.tr>
2023-02-20 17:09:24 +0300

    02

---
+++ ex02/fixed.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <ostream>
+
+class fixed {
+private:
+	int					raw_bits;
+	static const int	fractional = 8;
+public:
+	fixed();
+	fixed(const int raw);
+	fixed(const float raw);
+	fixed(fixed const &copy);
+	~fixed();
+	fixed&	operator=(const fixed&);
+	bool	operator>(const fixed& one);
+	int		getRawBits(void) const;
+	void	setRawBits(int const raw);
+	int		toInt(void) const ;
+};
+
+std::ostream&	operator<<(std::ostream &out, const fixed &copy);
