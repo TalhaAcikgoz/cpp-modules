9436a6ef626a06a0937c296160ac9f7fcedd8582
Talha Acikgoz <tacikgoz58@gmail.com>
2023-03-28 12:24:02 +0300

    abori

--- cpp01/ex03/humanA.cpp
+++
@@ -1,8 +0,0 @@
-#include "humanA.hpp"
-
-humanA::humanA(std::string name, weapon& weapon): _name(name), _weapon(weapon) {}
-humanA::~humanA() {}
-
-void    humanA::attack(void) const {
-    std::cout << this->_name << " attacks with their " << this->_weapon.getType() << std::endl;
-}
--- cpp01/ex04/replace.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <iostream>
-#include <fstream>
-#include <string>
-
-std::string replace(std::string line, std::string s1, std::string s2);
